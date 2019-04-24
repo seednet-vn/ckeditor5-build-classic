af0d1c97f368ecd39e09774c6cc6bc6c4a29d186
Phan Khắc Đạo <daopk2@vng.com.vn>
2019-04-24 15:21:57 +0700

    updated

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -4,45 +4,45 @@
  */
 
 // The editor creator to use.
-import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor"
 
-import Font from '@ckeditor/ckeditor5-font/src/font';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Font from "@ckeditor/ckeditor5-font/src/font"
+import Table from "@ckeditor/ckeditor5-table/src/table"
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar"
 
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
-import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
-import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
-import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold"
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic"
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline"
+import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough"
+import Code from "@ckeditor/ckeditor5-basic-styles/src/code"
+import Subscript from "@ckeditor/ckeditor5-basic-styles/src/subscript"
+import Superscript from "@ckeditor/ckeditor5-basic-styles/src/superscript"
 
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Heading from "@ckeditor/ckeditor5-heading/src/heading"
 
-import Link from '@ckeditor/ckeditor5-link/src/link'
-import List from '@ckeditor/ckeditor5-list/src/list'
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
+import Link from "@ckeditor/ckeditor5-link/src/link"
+import List from "@ckeditor/ckeditor5-list/src/list"
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote"
 
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import UploadAdapter from './uploadadapter';
+import Image from "@ckeditor/ckeditor5-image/src/image"
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption"
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle"
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar"
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload"
+import UploadAdapter from "./uploadadapter"
 
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials"
+import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed"
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment"
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice"
 
-import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
+import Highlight from "@ckeditor/ckeditor5-highlight/src/highlight"
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
-	Font,
+    Font,
     Table,
     TableToolbar,
     Bold,
@@ -67,42 +67,38 @@ ClassicEditor.builtinPlugins = [
     Alignment,
     PasteFromOffice,
     Highlight
-];
+]
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
-	toolbar: {
-		items: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'imageUpload',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
-		]
-	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
-	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
-};
+    toolbar: {
+        items: [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "link",
+            "bulletedList",
+            "numberedList",
+            "imageUpload",
+            "blockQuote",
+            "insertTable",
+            "mediaEmbed",
+            "undo",
+            "redo"
+        ]
+    },
+    image: {
+        toolbar: [
+            "imageStyle:full",
+            "imageStyle:side",
+            "|",
+            "imageTextAlternative"
+        ]
+    },
+    table: {
+        contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"]
+    },
+    // This value must be kept in sync with the language defined in webpack.config.js.
+    language: "en"
+}
--- src/uploadadapter.js
+++ src/uploadadapter.js
@@ -1,34 +1,35 @@
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import FileRepository from '@ckeditor/ckeditor5-upload/src/filerepository';
+import Plugin from "@ckeditor/ckeditor5-core/src/plugin"
+import FileRepository from "@ckeditor/ckeditor5-upload/src/filerepository"
 
 export default class SeednetUploadAdapter extends Plugin {
     /**
      * @inheritDoc
      */
     static get requires() {
-        return [ FileRepository ];
+        return [FileRepository]
     }
 
     /**
      * @inheritDoc
      */
     static get pluginName() {
-        return 'SeednetUploadAdapter';
+        return "SeednetUploadAdapter"
     }
 
     /**
      * @inheritDoc
      */
     init() {
-        const url = this.editor.config.get( 'upload.uploadUrl' );
-        const token = this.editor.config.get( 'upload.token' );
+        const url = this.editor.config.get("upload.uploadUrl")
+        const token = this.editor.config.get("upload.token")
 
-        if ( !url ) {
-            return;
+        if (!url) {
+            return
         }
 
         // Register SeednetAdapter
-        this.editor.plugins.get( FileRepository ).createUploadAdapter = loader => new UploadAdapter( loader, url, token, this.editor.t );
+        this.editor.plugins.get(FileRepository).createUploadAdapter = loader =>
+            new UploadAdapter(loader, url, token, this.editor.t)
     }
 }
 
@@ -46,20 +47,20 @@ class UploadAdapter {
      * @param {String} url
      * @param {module:utils/locale~Locale#t} t
      */
-    constructor( loader, url, token, t ) {
+    constructor(loader, url, token, t) {
         /**
          * FileLoader instance to use during the upload.
          *
          * @member {module:upload/filerepository~FileLoader} #loader
          */
-        this.loader = loader;
+        this.loader = loader
 
         /**
          * Upload URL.
          *
          * @member {String} #url
          */
-        this.url = url;
+        this.url = url
 
         this.token = token
 
@@ -68,7 +69,7 @@ class UploadAdapter {
          *
          * @member {module:utils/locale~Locale#t} #t
          */
-        this.t = t;
+        this.t = t
     }
 
     /**
@@ -78,12 +79,14 @@ class UploadAdapter {
      * @returns {Promise}
      */
     upload() {
-        return this.loader.file
-            .then( file => new Promise( ( resolve, reject ) => {
-                this._initRequest();
-                this._initListeners( resolve, reject, file );
-                this._sendRequest( file );
-            } ) );
+        return this.loader.file.then(
+            file =>
+                new Promise((resolve, reject) => {
+                    this._initRequest()
+                    this._initListeners(resolve, reject, file)
+                    this._sendRequest(file)
+                })
+        )
     }
 
     /**
@@ -93,8 +96,8 @@ class UploadAdapter {
      * @returns {Promise}
      */
     abort() {
-        if ( this.xhr ) {
-            this.xhr.abort();
+        if (this.xhr) {
+            this.xhr.abort()
         }
     }
 
@@ -104,10 +107,10 @@ class UploadAdapter {
      * @private
      */
     _initRequest() {
-        const xhr = this.xhr = new XMLHttpRequest();
+        const xhr = (this.xhr = new XMLHttpRequest())
 
-        xhr.open( 'POST', this.url, true );
-        xhr.responseType = 'json';
+        xhr.open("POST", this.url, true)
+        xhr.responseType = "json"
     }
 
     /**
@@ -117,35 +120,39 @@ class UploadAdapter {
      * @param {Function} resolve Callback function to be called when the request is successful.
      * @param {Function} reject Callback function to be called when the request cannot be completed.
      */
-    _initListeners( resolve, reject, file ) {
-        const xhr = this.xhr;
-        const loader = this.loader;
-        const t = this.t;
-        const genericError = t( 'Cannot upload file:' ) + ` ${ file.name }.`;
+    _initListeners(resolve, reject, file) {
+        const xhr = this.xhr
+        const loader = this.loader
+        const t = this.t
+        const genericError = t("Cannot upload file:") + ` ${file.name}.`
 
-        xhr.addEventListener( 'error', () => reject( genericError ));
-        xhr.addEventListener( 'abort', () => reject() );
-        xhr.addEventListener( 'load', () => {
-            const response = xhr.response;
+        xhr.addEventListener("error", () => reject(genericError))
+        xhr.addEventListener("abort", () => reject())
+        xhr.addEventListener("load", () => {
+            const response = xhr.response
 
-            if ( !response || !response.url ) {
-                return reject( response && response.error && response.error.message ? response.error.message : genericError );
+            if (!response || !response.url) {
+                return reject(
+                    response && response.error && response.error.message
+                        ? response.error.message
+                        : genericError
+                )
             }
 
-            resolve( {
+            resolve({
                 default: response.url
-            } );
-        } );
+            })
+        })
 
         // Upload progress when it's supported.
         /* istanbul ignore else */
-        if ( xhr.upload ) {
-            xhr.upload.addEventListener( 'progress', evt => {
-                if ( evt.lengthComputable ) {
-                    loader.uploadTotal = evt.total;
-                    loader.uploaded = evt.loaded;
+        if (xhr.upload) {
+            xhr.upload.addEventListener("progress", evt => {
+                if (evt.lengthComputable) {
+                    loader.uploadTotal = evt.total
+                    loader.uploaded = evt.loaded
                 }
-            } );
+            })
         }
     }
 
@@ -154,14 +161,14 @@ class UploadAdapter {
      *
      * @private
      */
-    _sendRequest(file ) {
+    async _sendRequest(file) {
         // Prepare form data.
-        const data = new FormData();
-        data.append( 'upload', file );
+        const data = new FormData()
+        data.append("upload", await file)
 
         // this.xhr.setRequestHeader('Authorization', 'Bearer ' + this.token())
 
         // Send request.
-        this.xhr.send( data );
+        this.xhr.send(data)
     }
 }
